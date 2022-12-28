b67e455938b86ce1d83eac433f995856af8086c4
rushoo <603370603@qq.com>
2022-12-28 16:58:01 +0800

    1、命令è通过通过命令行定义参数 2、自定义server以使用自定义的端口参数â

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 const (
-	env     = "development"
 	version = "1.0.0"
 )
 
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
 func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	app := &application{
+		config: cfg,
+		logger: log.New(os.Stdout, "", log.Ldate|log.Ltime),
+	}
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", healthCheckHandler)
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/v1/healthcheck", app.healthCheckHandler)
+
+	//自定义server以使用自定义的port
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.port),
+		Handler: mux,
+	}
 	log.Println("start server at http://localhost:4000")
-	log.Fatal(http.ListenAndServe(":4000", mux))
+	log.Fatal(server.ListenAndServe())
 }
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", env)
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
-}
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello,home page"))
 }
